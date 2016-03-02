bcb40af77f1be9b3f3fa49a8045f44110bcfbbf0
jaswsinc <jaswsinc@wsharks.com>
2016-03-02 13:23:32 -0900

    Updating build.

--- plugin.php
+++ plugin.php
@@ -1,18 +1,22 @@
 <?php
-/*
-Version: 160229
-Text Domain: wps-s2member-x
-Plugin Name: s2Member X Pro
-
-Author: WP Sharks™
-Author URI: https://wpsharks.com/
-
-License: GPL-3.0+
-License URI: http://www.gnu.org/licenses/gpl-3.0.html
-
-Plugin URI: https://wpsharks.com/product/s2member-x/
-Description: Membership functionality for WooCommerce.
-*/
+/**
+ * Plugin.
+ *
+ * @wp-plugin
+ *
+ * Version: 160302
+ * Text Domain: wps-s2member-x
+ * Plugin Name: s2Member X Pro
+ *
+ * Author: WP Sharks™
+ * Author URI: https://wpsharks.com/
+ *
+ * License: GPL-3.0+
+ * License URI: http://www.gnu.org/licenses/gpl-3.0.html
+ *
+ * Plugin URI: https://wpsharks.com/product/s2member-x/
+ * Description: Membership functionality for WooCommerce.
+ */
 if (!defined('WPINC')) {
     exit('Do NOT access this file directly: '.basename(__FILE__));
 }
--- readme.txt
+++ readme.txt
@@ -1,6 +1,6 @@
 === s2Member X Pro ===
 
-Stable tag: 160229
+Stable tag: 160302
 Requires at least: 4.4
 Tested up to: 4.4
 Text Domain: wps-s2member-x
