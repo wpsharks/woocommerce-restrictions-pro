5fae4998e0b6e0b64428e34d235079f187ef8e92
jaswsinc <jaswsinc@wsharks.com>
2016-03-03 14:52:15 -0900

    Enhancing stubs.

--- src/includes/plugin.php
+++ src/includes/plugin.php
@@ -7,13 +7,12 @@ use WebSharks\WpSharks\s2MemberX\Pro\Classes\App;
 if (!defined('WPINC')) {
     exit('Do NOT access this file directly: '.basename(__FILE__));
 }
-require __DIR__.'/rv.php'; // Setup `wp_sharks_core_rv` config.
-
-if (require(dirname(__DIR__).'/vendor/websharks/wp-sharks-core-rv/src/includes/check.php')) {
-    add_action('plugins_loaded', function () {
+add_action('plugins_loaded', function () {
+    require __DIR__.'/rv.php'; // Setup `wp_sharks_core_rv` config.
+    if (require(dirname(__DIR__).'/vendor/websharks/wp-sharks-core-rv/src/includes/check.php')) {
         require_once __DIR__.'/stub.php';
         new App();
-    });
-} else {
-    wp_php_rv_notice('s2Member® X Pro');
-}
+    } else {
+        wp_php_rv_notice('s2Member® X Pro');
+    }
+});
