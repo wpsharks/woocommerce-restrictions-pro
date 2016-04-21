22141f1486972a7565d6b00089ec142e0bbffc4a
jaswsinc <jaswsinc@wsharks.com>
2016-04-21 07:22:44 -0800

    @TODO `RestrictionMeta{}`.

---
+++ src/includes/classes/Utils/RestrictionMeta.php
@@ -0,0 +1,30 @@
+<?php
+declare (strict_types = 1);
+namespace WebSharks\WpSharks\s2MemberX\Pro\Classes\Utils;
+
+use WebSharks\WpSharks\s2MemberX\Pro\Classes;
+use WebSharks\WpSharks\s2MemberX\Pro\Interfaces;
+use WebSharks\WpSharks\s2MemberX\Pro\Traits;
+#
+use WebSharks\WpSharks\s2MemberX\Pro\Classes\AppFacades as a;
+use WebSharks\WpSharks\s2MemberX\Pro\Classes\SCoreFacades as s;
+use WebSharks\WpSharks\s2MemberX\Pro\Classes\CoreFacades as c;
+#
+use WebSharks\WpSharks\Core\Classes as SCoreClasses;
+use WebSharks\WpSharks\Core\Interfaces as SCoreInterfaces;
+use WebSharks\WpSharks\Core\Traits as SCoreTraits;
+#
+use WebSharks\Core\WpSharksCore\Classes as CoreClasses;
+use WebSharks\Core\WpSharksCore\Classes\Core\Base\Exception;
+use WebSharks\Core\WpSharksCore\Interfaces as CoreInterfaces;
+use WebSharks\Core\WpSharksCore\Traits as CoreTraits;
+
+/**
+ * Restriction meta.
+ *
+ * @since 16xxxx Installer.
+ */
+class RestrictionMeta extends SCoreClasses\SCore\Base\Core
+{
+    // @TODO Restriction meta boxes.
+}
