ee21deb1b235fd25c576f2c0db9b27c4aa7b5216
jaswsinc <jaswsinc@wsharks.com>
2016-04-27 11:19:14 -0800

    Outlining `SecurityGate{}` class.

--- src/includes/classes/Utils/SecurityGate.php
+++ src/includes/classes/Utils/SecurityGate.php
@@ -29,7 +29,7 @@ class SecurityGate extends SCoreClasses\SCore\Base\Core
     /**
      * Class constructor.
      *
-     * @since 16xxxx Restrictions.
+     * @since 16xxxx Security gate.
      *
      * @param Classes\App $App Instance.
      */
@@ -41,9 +41,76 @@ class SecurityGate extends SCoreClasses\SCore\Base\Core
     /**
      * Guard restrictions.
      *
-     * @since 16xxxx Restrictions.
+     * @since 16xxxx Security gate.
      */
     public function onInitGuardRestrictions()
     {
+        $this->maybeGuardSingularAccess();
+        $this->maybeGuardCategoryArchiveAccess();
+        $this->maybeGuardTagArchiveAccess();
+        $this->maybeGuardTaxArchiveAccess();
+        $this->maybeGuardOtherAccess();
     }
+
+    /**
+     * Singular.
+     *
+     * @since 16xxxx Security gate.
+     */
+    protected function maybeGuardSingularAccess()
+    {
+        if (!is_singular()) {
+            return; // Not applicable.
+        }
+    }
+
+    /**
+     * Category archive.
+     *
+     * @since 16xxxx Security gate.
+     */
+    protected function maybeGuardCategoryArchiveAccess()
+    {
+        if (!is_category()) {
+            return; // Not applicable.
+        }
+    }
+
+    /**
+     * Tag archive.
+     *
+     * @since 16xxxx Security gate.
+     */
+    protected function maybeGuardTagArchiveAccess()
+    {
+        if (!is_tag()) {
+            return; // Not applicable.
+        }
+    }
+
+    /**
+     * Custom taxonomy archive.
+     *
+     * @since 16xxxx Security gate.
+     */
+    protected function maybeGuardTaxArchiveAccess()
+    {
+        if (!is_tax()) {
+            return; // Not applicable.
+        }
+    }
+
+    /**
+     * Anything else; e.g., URIs.
+     *
+     * @since 16xxxx Security gate.
+     */
+    protected function maybeGuardOtherAccess()
+    {
+    }
+
+    // @TODO Decide how to handle author and date-based archives, if at all.
+    //  This will probably be something that a site owner should cover on their own via URI Patterns.
+    // I would like to avoid needing to deal with the Alternative Views concept in the old software.
+    // `is_date()`, `is_author()`.
 }
