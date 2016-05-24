32b718223fd01ae8252ab8e3a1e98a40b0b0cb79
jaswsinc <jaswsinc@wsharks.com>
2016-05-24 04:11:47 -0800

    Updating build.

--- src/includes/wp-php-rv.php
+++ src/includes/wp-php-rv.php
@@ -5,10 +5,15 @@ if (!defined('WPINC')) {
     exit('Do NOT access this file directly: '.basename(__FILE__));
 }
 $GLOBALS['wp_php_rv'] = array(
-    'os'         => '', //os-required//
+    'os' => '', //os-required//
+
     'min'        => '7.0.4', //php-required-version//
     'bits'       => 64, //php-required-bits//
     'extensions' => array(), //php-required-extensions//
+
+    'wp' => array(
+        'min' => '4.5.2', //wp-required-version//
+    ),
 ); // The following keys are for back compat. only.
 $GLOBALS['wp_php_rv']['rv'] = $GLOBALS['wp_php_rv']['min'];
 $GLOBALS['wp_php_rv']['re'] = $GLOBALS['wp_php_rv']['extensions'];
